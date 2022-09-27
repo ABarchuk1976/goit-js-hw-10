94b4220b333f1dba5cb33c042be7a695a1d04c71
Barchuk Andrii <abarchuk1976@gmail.com>
2022-09-27 23:47:23 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
 import { debounce } from 'lodash';
+const _ = require('lodash');
 import Notiflix from 'notiflix';
 
 Notiflix.Report.init({
@@ -9,4 +10,14 @@ Notiflix.Report.init({
   backOverlayClickToClose: true,
 });
 
+const inputRef = document.querySelector('#search-box');
+const countryListRef = document.querySelector('.country-list');
+const countryInfoRef = document.querySelector('.country-info');
+
 const DEBOUNCE_DELAY = 300;
+
+inputRef.addEventListener('input', event => {
+  if (event.currentTarget.value === '') return clearAll();
+
+  _.debounce(() => {}, DEBOUNCE_DELAY);
+});
