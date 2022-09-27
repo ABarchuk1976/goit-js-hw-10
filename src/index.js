d815edd40e6a86592e8eeea2d9f21e0965381c70
Barchuk Andrii <abarchuk1976@gmail.com>
2022-09-27 22:43:41 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import './css/styles.css';
 import { debounce } from 'lodash';
+import Notiflix from 'notiflix';
+
+Notiflix.Report.init({
+  svgSize: '48px',
+  messageFontSize: '16px',
+  buttonFontSize: '16px',
+  backOverlayClickToClose: true,
+});
 
 const DEBOUNCE_DELAY = 300;
